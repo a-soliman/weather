21000e4a5334a7cbfe73bab0421a4b768471591c
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-06-22 10:02:38 -0700

    pulling the submitted data and validating against empty string

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 const ui = new UI();
 
 document.querySelector('#w-change-btn').addEventListener('click', (e) => {
+    const city = document.querySelector('#city').value.trim();
+    const country = document.querySelector('#country').value.trim();
+    
+    if ( city == '' || country == '' ) {
+        // DISPLAY VALIDATION ERROR
+        
+        return;
+    }
     
 });
