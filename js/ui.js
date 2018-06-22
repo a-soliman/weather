a828048dad4971281ade7c2b82953bea2a9be79d
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-06-22 13:18:24 -0700

    code showWeather method

--- js/ui.js
+++ js/ui.js
@@ -3,6 +3,38 @@ class UI {
 
     showWeather(data) {
         console.log('Showing the weather');
+        console.log(data);
+        const location = `${data.city.name}, ${data.city.country}`;
+        const temp = Math.floor(data.list["0"].main.temp - 273.15);
+        const description = data.list["0"].weather["0"].description;
+        const icon = `http://openweathermap.org/img/w/${data.list["0"].weather["0"].icon}.png`;
+        const humidity = data.list["0"].main.humidity;
+        const max = Math.floor(data.list["0"].main.temp_max - 273.15);
+        const min = Math.floor(data.list["0"].main.temp_min - 273.15);
+        const windSpeed = data.list["0"].wind.speed;
+
+        const locationContainer = document.getElementById('w-location');
+        const descriptionContainer = document.getElementById('w-desc');
+        const tempContainer         = document.getElementById('w-string');
+        const iconContainer = document.getElementById('w-icon');
+        const humidityContainer = document.getElementById('w-humidity');
+        const maxContainer = document.getElementById('w-max');
+        const minContainer = document.getElementById('w-min');
+        const windSpeedContainer = document.getElementById('w-wind');
+
+        locationContainer.innerText     = location;
+        descriptionContainer.innerText  = description;
+        tempContainer.innerText         = temp;
+        humidityContainer.innerText     = humidity;
+        maxContainer.innerText          = max;
+        minContainer.innerText          = min;
+        windSpeedContainer.innerText    = windSpeed;
+        
+
+
+
+        iconContainer.setAttribute('src', icon);
+
     }
 
     clearInputs() {
