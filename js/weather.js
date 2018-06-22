1b9ccb4361243bd55c36120920cf9010e90a677a
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-06-22 09:08:43 -0700

    testing the weather api

---
+++ js/weather.js
@@ -0,0 +1,43 @@
+class Weather {
+    constructor(city, country) {
+        this.api_key = '37d499bd543bde314ffe9d6c454b5388';
+        this.city = city;
+        this.country = country;
+    }
+
+    getCityId() {
+        return fetch('/city.list.json')
+            .then((response) => response.json())
+            .then((data) => {
+                let city = data.filter((city) => {
+                    return city.name.toLowerCase() == this.city.toLowerCase() && city.country.toLowerCase() == this.country.toLowerCase();
+                })[0];
+                
+                if (city) {
+                    console.log('yes');
+                    return city.id;
+                }
+                return false;
+            })
+            .catch(err => err);
+    }
+
+    getWeather() {
+        this.getCityId()
+            .then((city_id) => {
+                if( city_id ) {
+                    return fetch(`http://api.openweathermap.org/data/2.5/forecast?id=${city_id}&APPID=${this.api_key}`)
+                        .then((response) => response.json())
+                        .then((data) => {
+                            console.log(data);
+                            return data;
+                        })
+                        .catch((err) => err);
+                }
+            })
+    }
+    
+}
+
+let test = new Weather('San Francisco', 'US');
+test.getWeather();
