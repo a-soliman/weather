8bb9783423ee694d590fbc8e62b1a93837c28220
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-06-22 09:59:25 -0700

    outlined ui methods

--- js/ui.js
+++ js/ui.js
@@ -1,3 +1,15 @@
 class UI {
     constructor() {}
+
+    showWeather(data) {
+        console.log('Showing the weather');
+    }
+
+    clearInputs() {
+        console.log('clearing inputs');
+    }
+
+    closeModal() {
+        console.log('closing modal');
+    }
 }
